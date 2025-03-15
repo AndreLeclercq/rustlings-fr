53c64aea7a8014f8d91ca86c718daf5111e2a146
Andre LECLERCQ <andre.leclercq@oclock.io>
2025-03-15 22:00:24 +0100

    :globe_with_meridians: [solutions] 06 move semantics

--- solutions/06_move_semantics/move_semantics5.rs
+++ solutions/06_move_semantics/move_semantics5.rs
@@ -1,4 +1,23 @@
+#![allow(clippy::ptr_arg)]
+
+// Emprunte au lieu de prendre possession.
+// Il est recommandé d'utiliser `&str` au lieu de `&String` ici. Mais c'est
+// suffisant pour l'instant car nous n'avons pas encore géré les strings (chaînes).
+fn get_char(data: &String) -> char {
+   data.chars().last().unwrap()
+}
+
+// Prend possession au lieu d'emprunter.
+fn string_uppercase(mut data: String) {
+   data = data.to_uppercase();
+
+   println!("{data}");
+}
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+   let data = "Rust est génial!".to_string();
+
+   get_char(&data);
+
+   string_uppercase(data);
 }
