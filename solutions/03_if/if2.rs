2d6c9f8d611f90fdf39eaf432d601456de2766f4
Andre LECLERCQ <andre.leclercq@oclock.io>
2025-03-15 20:09:20 +0100

    :globe_with_meridians: [solutions] 03 if

--- solutions/03_if/if2.rs
+++ solutions/03_if/if2.rs
@@ -1,4 +1,35 @@
+fn picky_eater(food: &str) -> &str {
+    if food == "fraise" {
+        "Miam !"
+    } else if food == "patate" {
+        "Je suppose que je peux manger ça."
+    } else {
+        "Non merci !"
+    }
+}
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+    // Tu peux expérimenter ici si tu veux.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn yummy_food() {
+        assert_eq!(picky_eater("fraise"), "Miam !");
+    }
+
+    #[test]
+    fn neutral_food() {
+        assert_eq!(picky_eater("patate"), "Je suppose que je peux manger ça.");
+    }
+
+    #[test]
+    fn default_disliked_food() {
+        assert_eq!(picky_eater("brocoli"), "Non merci !");
+        assert_eq!(picky_eater("bonbons"), "Non merci !");
+        assert_eq!(picky_eater("n'importe quoi"), "Non merci !");
+    }
 }
--- solutions/03_if/if3.rs
+++ solutions/03_if/if3.rs
@@ -1,4 +1,53 @@
+fn animal_habitat(animal: &str) -> &str {
+    let identifier = if animal == "crabe" {
+        1
+    } else if animal == "spermophile" {
+        2
+    } else if animal == "serpent" {
+        3
+    } else {
+        // Un identifiant non utilisé.
+        4
+    };
+
+    // Normalement on utiliserait une enum (énumération) en Rust.
+    // Mais nous n'avons pas encore abordé les enums.
+    if identifier == 1 {
+        "Plage"
+    } else if identifier == 2 {
+        "Terrier"
+    } else if identifier == 3 {
+        "Désert"
+    } else {
+        "Inconnu"
+    }
+}
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+    // Tu peux expérimenter ici si tu veux.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn le_gaufre_vit_dans_un_terrier() {
+        assert_eq!(animal_habitat("spermophile"), "Terrier")
+    }
+
+    #[test]
+    fn le_serpent_vit_dans_le_desert() {
+        assert_eq!(animal_habitat("serpent"), "Désert")
+    }
+
+    #[test]
+    fn le_crabe_vit_sur_la_plage() {
+        assert_eq!(animal_habitat("crabe"), "Plage")
+    }
+
+    #[test]
+    fn animal_inconnu() {
+        assert_eq!(animal_habitat("dinosaure"), "Inconnu")
+    }
 }
