913a8f0cafe7bb774c40ab77fb831da0a3fcfde6
Andre LECLERCQ <andre.leclercq@oclock.io>
2025-03-16 13:21:40 +0100

    :bug: Hotfix [Solutions] 09 strings

--- solutions/09_strings/strings3.rs
+++ solutions/09_strings/strings3.rs
@@ -1,4 +1,48 @@
+fn trim_me(input: &str) -> &str {
+   input.trim()
+}
+
+fn compose_me(input: &str) -> String {
+   // La macro `format!` a la même syntaxe que `println!`, mais elle renvoie une
+   // chaîne au lieu de l'afficher dans le terminal.
+   // Équivalent à `input.to_string() + " world!"`
+   format!("{input} world!")
+}
+
+fn replace_me(input: &str) -> String {
+   input.replace("cars", "balloons")
+}
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+   // Tu peux expérimenter ici si tu veux.
+}
+
+#[cfg(test)]
+mod tests {
+   use super::*;
+
+   #[test]
+   fn trim_a_string() {
+       assert_eq!(trim_me("Hello!     "), "Hello!");
+       assert_eq!(trim_me("  What's up!"), "What's up!");
+       assert_eq!(trim_me("   Hola!  "), "Hola!");
+   }
+
+   #[test]
+   fn compose_a_string() {
+       assert_eq!(compose_me("Hello"), "Hello world!");
+       assert_eq!(compose_me("Goodbye"), "Goodbye world!");
+   }
+
+   #[test]
+   fn replace_a_string() {
+       assert_eq!(
+           replace_me("I think cars are cool"),
+           "I think balloons are cool",
+       );
+       assert_eq!(
+           replace_me("I love to look at cars"),
+           "I love to look at balloons",
+       );
+   }
 }
