3f72aee18694e37ef82fb352134d04644b51102a
Andre LECLERCQ <andre.leclercq@oclock.io>
2025-03-16 16:21:13 +0100

    :globe_with_meridians: [solutions] 15 traits

--- solutions/15_traits/traits3.rs
+++ solutions/15_traits/traits3.rs
@@ -1,4 +1,36 @@
+trait Licensed {
+    fn licensing_info(&self) -> String {
+        "Licence par défaut".to_string()
+    }
+}
+
+struct SomeSoftware {
+    version_number: i32,
+}
+
+struct OtherSoftware {
+    version_number: String,
+}
+
+impl Licensed for SomeSoftware {}
+impl Licensed for OtherSoftware {}
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+    // Tu peux expérimenter ici si tu le souhaites.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_licensing_info_the_same() {
+        let licensing_info = "Licence par défaut";
+        let some_software = SomeSoftware { version_number: 1 };
+        let other_software = OtherSoftware {
+            version_number: "v2.0.0".to_string(),
+        };
+        assert_eq!(some_software.licensing_info(), licensing_info);
+        assert_eq!(other_software.licensing_info(), licensing_info);
+    }
 }
