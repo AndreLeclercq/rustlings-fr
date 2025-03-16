a36c40ea97ecdb9b864eb123b7d58543fc3df1ad
Andre LECLERCQ <andre.leclercq@oclock.io>
2025-03-16 14:36:59 +0100

    :globe_with_meridians: [exercises] 11 hashmaps

--- exercises/11_hashmaps/hashmaps1.rs
+++ exercises/11_hashmaps/hashmaps1.rs
@@ -1,25 +1,25 @@
-// A basket of fruits in the form of a hash map needs to be defined. The key
-// represents the name of the fruit and the value represents how many of that
-// particular fruit is in the basket. You have to put at least 3 different
-// types of fruits (e.g. apple, banana, mango) in the basket and the total count
-// of all the fruits should be at least 5.
+// Un panier de fruits sous forme de hash map (table de hachage) doit être défini. La clé
+// représente le nom du fruit et la valeur représente combien de ce
+// fruit particulier se trouve dans le panier. Tu dois mettre au moins 3 différents
+// types de fruits (par exemple pomme, banane, mangue) dans le panier et le nombre total
+// de tous les fruits doit être au moins 5.
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    // TODO: Declare the hash map.
+    // TODO: Déclare la hash map.
     // let mut basket =
 
-    // Two bananas are already given for you :)
-    basket.insert(String::from("banana"), 2);
+    // Deux bananes sont déjà là pour toi :)
+    basket.insert(String::from("banane"), 2);
 
-    // TODO: Put more fruits in your basket.
+    // TODO: Mets plus de fruits dans ton panier.
 
     basket
 }
 
 fn main() {
-    // You can optionally experiment here.
+    // Tu peux expérimenter ici si tu veux.
 }
 
 #[cfg(test)]
--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -1,97 +1,97 @@
-// We're collecting different fruits to bake a delicious fruit cake. For this,
-// we have a basket, which we'll represent in the form of a hash map. The key
-// represents the name of each fruit we collect and the value represents how
-// many of that particular fruit we have collected. Three types of fruits -
-// Apple (4), Mango (2) and Lychee (5) are already in the basket hash map. You
-// must add fruit to the basket so that there is at least one of each kind and
-// more than 11 in total - we have a lot of mouths to feed. You are not allowed
-// to insert any more of the fruits that are already in the basket (Apple,
-// Mango, and Lychee).
+// Nous collectons différents fruits pour préparer un délicieux gâteau aux fruits. Pour cela,
+// nous avons un panier, que nous allons représenter sous forme de hash map (table de hachage). La clé
+// représente le nom de chaque fruit que nous collectons et la valeur représente combien
+// de ce fruit particulier nous avons collecté. Trois types de fruits -
+// Pomme (4), Mangue (2) et Litchi (5) sont déjà dans le panier. Tu
+// dois ajouter des fruits au panier pour qu'il y ait au moins un de chaque type et
+// plus de 11 au total - nous avons beaucoup de bouches à nourrir. Tu n'as pas le droit
+// d'ajouter plus des fruits qui sont déjà dans le panier (Pomme,
+// Mangue et Litchi).
 
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq, Debug)]
 enum Fruit {
-    Apple,
-    Banana,
-    Mango,
-    Lychee,
-    Pineapple,
+   Pomme,
+   Banane,
+   Mangue,
+   Litchi,
+   Ananas,
 }
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
-    let fruit_kinds = [
-        Fruit::Apple,
-        Fruit::Banana,
-        Fruit::Mango,
-        Fruit::Lychee,
-        Fruit::Pineapple,
-    ];
+   let fruit_kinds = [
+       Fruit::Pomme,
+       Fruit::Banane,
+       Fruit::Mangue,
+       Fruit::Litchi,
+       Fruit::Ananas,
+   ];
 
-    for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
-        // basket. Note that you are not allowed to put any type of fruit that's
-        // already present!
-    }
+   for fruit in fruit_kinds {
+       // TODO: Insère de nouveaux fruits s'ils ne sont pas déjà présents dans le
+       // panier. Note que tu n'as pas le droit d'ajouter un type de fruit qui est
+       // déjà présent !
+   }
 }
 
 fn main() {
-    // You can optionally experiment here.
+   // Tu peux expérimenter ici si tu veux.
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+   use super::*;
 
-    // Don't modify this function!
-    fn get_fruit_basket() -> HashMap<Fruit, u32> {
-        let content = [(Fruit::Apple, 4), (Fruit::Mango, 2), (Fruit::Lychee, 5)];
-        HashMap::from_iter(content)
-    }
+   // Ne modifie pas cette fonction !
+   fn get_fruit_basket() -> HashMap<Fruit, u32> {
+       let content = [(Fruit::Pomme, 4), (Fruit::Mangue, 2), (Fruit::Litchi, 5)];
+       HashMap::from_iter(content)
+   }
 
-    #[test]
-    fn test_given_fruits_are_not_modified() {
-        let mut basket = get_fruit_basket();
-        fruit_basket(&mut basket);
-        assert_eq!(*basket.get(&Fruit::Apple).unwrap(), 4);
-        assert_eq!(*basket.get(&Fruit::Mango).unwrap(), 2);
-        assert_eq!(*basket.get(&Fruit::Lychee).unwrap(), 5);
-    }
+   #[test]
+   fn test_given_fruits_are_not_modified() {
+       let mut basket = get_fruit_basket();
+       fruit_basket(&mut basket);
+       assert_eq!(*basket.get(&Fruit::Pomme).unwrap(), 4);
+       assert_eq!(*basket.get(&Fruit::Mangue).unwrap(), 2);
+       assert_eq!(*basket.get(&Fruit::Litchi).unwrap(), 5);
+   }
 
-    #[test]
-    fn at_least_five_types_of_fruits() {
-        let mut basket = get_fruit_basket();
-        fruit_basket(&mut basket);
-        let count_fruit_kinds = basket.len();
-        assert!(count_fruit_kinds >= 5);
-    }
+   #[test]
+   fn at_least_five_types_of_fruits() {
+       let mut basket = get_fruit_basket();
+       fruit_basket(&mut basket);
+       let count_fruit_kinds = basket.len();
+       assert!(count_fruit_kinds >= 5);
+   }
 
-    #[test]
-    fn greater_than_eleven_fruits() {
-        let mut basket = get_fruit_basket();
-        fruit_basket(&mut basket);
-        let count = basket.values().sum::<u32>();
-        assert!(count > 11);
-    }
+   #[test]
+   fn greater_than_eleven_fruits() {
+       let mut basket = get_fruit_basket();
+       fruit_basket(&mut basket);
+       let count = basket.values().sum::<u32>();
+       assert!(count > 11);
+   }
 
-    #[test]
-    fn all_fruit_types_in_basket() {
-        let fruit_kinds = [
-            Fruit::Apple,
-            Fruit::Banana,
-            Fruit::Mango,
-            Fruit::Lychee,
-            Fruit::Pineapple,
-        ];
+   #[test]
+   fn all_fruit_types_in_basket() {
+       let fruit_kinds = [
+           Fruit::Pomme,
+           Fruit::Banane,
+           Fruit::Mangue,
+           Fruit::Litchi,
+           Fruit::Ananas,
+       ];
 
-        let mut basket = get_fruit_basket();
-        fruit_basket(&mut basket);
+       let mut basket = get_fruit_basket();
+       fruit_basket(&mut basket);
 
-        for fruit_kind in fruit_kinds {
-            let Some(amount) = basket.get(&fruit_kind) else {
-                panic!("Fruit kind {fruit_kind:?} was not found in basket");
-            };
-            assert!(*amount > 0);
-        }
-    }
+       for fruit_kind in fruit_kinds {
+           let Some(amount) = basket.get(&fruit_kind) else {
+               panic!("Le type de fruit {fruit_kind:?} n'a pas été trouvé dans le panier");
+           };
+           assert!(*amount > 0);
+       }
+   }
 }
