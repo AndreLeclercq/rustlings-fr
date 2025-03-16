dc79213d12abd9f8ed28a0e73e079f980d3bda39
Andre LECLERCQ <andre.leclercq@oclock.io>
2025-03-16 13:17:00 +0100

    :globe_with_meridians: [solutions] 09 strings

--- solutions/09_strings/strings2.rs
+++ solutions/09_strings/strings2.rs
@@ -1,4 +1,15 @@
+fn is_a_color_word(attempt: &str) -> bool {
+   attempt == "green" || attempt == "blue" || attempt == "red"
+}
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+   let word = String::from("green");
+
+   if is_a_color_word(&word) {
+       //             ^ ajouté pour avoir `&String` qui est automatiquement
+       //               converti en `&str` par le compilateur.
+       println!("C'est une couleur que je connais !");
+   } else {
+       println!("Ce n'est pas une couleur que je connais.");
+   }
 }
