72a5e51ff101655ef4fe79556ccec455a27c4f36
Andre LECLERCQ <andre.leclercq@oclock.io>
2025-03-16 21:18:45 +0100

    :globe_with_meridians: [solutions] 20 threads

--- solutions/20_threads/threads2.rs
+++ solutions/20_threads/threads2.rs
@@ -1,4 +1,41 @@
+// En s'appuyant sur l'exercice précédent, nous voulons que tous les threads terminent leur
+// travail. Mais cette fois, les threads lancés doivent être responsables de la mise à jour d'une
+// valeur partagée : `JobStatus.jobs_done`
+
+use std::{
+   sync::{Arc, Mutex},
+   thread,
+   time::Duration,
+};
+
+struct JobStatus {
+   jobs_done: u32,
+}
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+   // `Arc` n'est pas suffisant si tu veux un état partagé **mutable**.
+   // Nous devons envelopper la valeur avec un `Mutex` (verrou d'exclusion mutuelle).
+   let status = Arc::new(Mutex::new(JobStatus { jobs_done: 0 }));
+   //                    ^^^^^^^^^^^                          ^
+
+   let mut handles = Vec::new();
+   for _ in 0..10 {
+       let status_shared = Arc::clone(&status);
+       let handle = thread::spawn(move || {
+           thread::sleep(Duration::from_millis(250));
+
+           // Verrouille avant de mettre à jour une valeur partagée.
+           status_shared.lock().unwrap().jobs_done += 1;
+           //           ^^^^^^^^^^^^^^^^
+       });
+       handles.push(handle);
+   }
+
+   // Attente que tous les travaux soient terminés.
+   for handle in handles {
+       handle.join().unwrap();
+   }
+
+   println!("Travaux terminés : {}", status.lock().unwrap().jobs_done);
+   //                                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 }
--- solutions/20_threads/threads3.rs
+++ solutions/20_threads/threads3.rs
@@ -1,4 +1,62 @@
+use std::{sync::mpsc, thread, time::Duration};
+
+struct Queue {
+   first_half: Vec<u32>,
+   second_half: Vec<u32>,
+}
+
+impl Queue {
+   fn new() -> Self {
+       Self {
+           first_half: vec![1, 2, 3, 4, 5],
+           second_half: vec![6, 7, 8, 9, 10],
+       }
+   }
+}
+
+fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
+   // Clone d'abord l'émetteur `tx`.
+   let tx_clone = tx.clone();
+   thread::spawn(move || {
+       for val in q.first_half {
+           println!("Envoi de {val:?}");
+           // Ensuite, utilise le clone dans le premier thread. Cela signifie que
+           // `tx_clone` est déplacé vers le premier thread et `tx` vers le second.
+           tx_clone.send(val).unwrap();
+           thread::sleep(Duration::from_millis(250));
+       }
+   });
+
+   thread::spawn(move || {
+       for val in q.second_half {
+           println!("Envoi de {val:?}");
+           tx.send(val).unwrap();
+           thread::sleep(Duration::from_millis(250));
+       }
+   });
+}
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+   // Tu peux expérimenter ici si tu le souhaites.
+}
+
+#[cfg(test)]
+mod tests {
+   use super::*;
+
+   #[test]
+   fn threads3() {
+       let (tx, rx) = mpsc::channel();
+       let queue = Queue::new();
+
+       send_tx(queue, tx);
+
+       let mut received = Vec::with_capacity(10);
+       for value in rx {
+           received.push(value);
+       }
+
+       received.sort();
+       assert_eq!(received, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+   }
 }
