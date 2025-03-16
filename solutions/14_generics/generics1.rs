b12e8124d7c1559995347a42a946e3343d4d1e76
Andre LECLERCQ <andre.leclercq@oclock.io>
2025-03-16 16:02:40 +0100

    :globe_with_meridians: [solutions] 14 generics

--- solutions/14_generics/generics1.rs
+++ solutions/14_generics/generics1.rs
@@ -1,4 +1,17 @@
+// `Vec<T>` est générique sur le type `T`. Dans la plupart des cas, le compilateur est capable de
+// déduire `T`, par exemple après avoir ajouté une valeur avec un type concret au vecteur.
+// Mais dans cet exercice, le compilateur a besoin d'un petit coup de pouce sous forme d'une annotation de type.
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+    // `u8` et `i8` peuvent tous deux être convertis en `i16`.
+    let mut numbers: Vec<i16> = Vec::new();
+    //             ^^^^^^^^^^ ajouté
+
+    // Ne change pas les lignes ci-dessous.
+    let n1: u8 = 42;
+    numbers.push(n1.into());
+    let n2: i8 = -1;
+    numbers.push(n2.into());
+
+    println!("{numbers:?}");
 }
