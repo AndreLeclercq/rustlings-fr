b4dd0edcb3bdfd6e72750b7fba97c175fdd58401
Andre LECLERCQ <andre.leclercq@oclock.io>
2025-03-16 17:06:36 +0100

    :globe_with_meridians: [solutions] 17 tests

--- solutions/17_tests/tests1.rs
+++ solutions/17_tests/tests1.rs
@@ -1,4 +1,22 @@
+// Les tests sont importants pour s'assurer que ton code fait bien ce que tu penses qu'il devrait faire.
+
+fn is_even(n: i64) -> bool {
+    n % 2 == 0
+}
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+    // Tu peux expérimenter ici si tu le souhaites.
+}
+
+#[cfg(test)]
+mod tests {
+    // Lors de l'écriture de tests unitaires (unit tests), il est courant d'importer tout ce qui vient du module parent (`super`) en utilisant un joker (*).
+    use super::*;
+
+    #[test]
+    fn tu_peux_affirmer() {
+        assert!(is_even(0));
+        assert!(!is_even(-1));
+        //      ^ Tu peux affirmer `false` en utilisant l'opérateur de négation `!`.
+    }
 }
