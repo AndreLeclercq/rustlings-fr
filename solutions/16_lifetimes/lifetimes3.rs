756c31c865750519415a0f7caa09b1ffc720c729
Andre LECLERCQ <andre.leclercq@oclock.io>
2025-03-16 16:44:51 +0100

    :globe_with_meridians: [solutions] 16 lifetimes

--- solutions/16_lifetimes/lifetimes3.rs
+++ solutions/16_lifetimes/lifetimes3.rs
@@ -1,4 +1,18 @@
+// Les durées de vie (lifetimes) sont aussi nécessaires quand les structs contiennent des références.
+
+struct Book<'a> {
+   //     ^^^^ annotation de durée de vie ajoutée
+   author: &'a str,
+   //       ^^
+   title: &'a str,
+   //      ^^
+}
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+   let book = Book {
+       author: "George Orwell",
+       title: "1984",
+   };
+
+   println!("{} par {}", book.title, book.author);
 }
